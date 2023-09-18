5250f78aefc761b335de7d7ba85655be76a1b66a
Saurav <alexthundrous2104@gmail.com>
2023-09-18 23:17:34 +0530

    Add text and basic animations

--- src/Components/Events/events.js
+++ src/Components/Events/events.js
@@ -1,26 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
     ChakraProvider,
     Box,
     theme,
     Image,
+    Flex,
+    Text
 } from '@chakra-ui/react';
-import bgimage from './bgimage.png';  
+import bgimage from './bgimage.png';
 import jellyfish from './jellyfish.svg'
 import horsewheel from './horsewheel.svg'
 import spartanhelmet from './spartanhelmet.svg'
+import helmet_animated from './helmet_animated.svg'
 import swords from './sword.svg'
+import sword_animated from './sword_animated.svg'
 import box from './box.svg'
+import box_animated from './box2.svg'
 import seaweed from './seaweed.svg'
 
 function Events() {
+    const [isHoveringSword, setIsHoveringSword] = useState(false);
+    const [isHoveringBox, setIsHoveringBox] = useState(false);
+    const [isHoveringHelmet, setIsHoveringHelmet] = useState(false);
     const zoomInOnHover = {
         transform: "scale(1)",
         transition: "transform 0.5s",
     };
 
     const zoomOutOnHover = {
-        transform: "scale(1.5)",
+        transform: "scale(1.3)",
         transition: "transform 0.5s",
         top: "8%",
     };
@@ -36,74 +44,99 @@ function Events() {
                     height="100vh"
                     objectFit="cover"
                 />
-                <Image
-                    src={jellyfish}
-                    alt="Jellyfish"
-                    position="absolute"
-                    top="5%"
-                    right="5%"
-                    width="314px"     
-                    height="221px"
-                    zIndex="1"
-                    style={jellyfishStyle}
-                    onMouseEnter={() => {
-                        setJellyfishStyle(zoomOutOnHover);
-                    }}
-                    onMouseLeave={() => {
-                        setJellyfishStyle(zoomInOnHover);
-                    }}
-                />
+                <Flex direction="column">
+                    <Box position="absolute" top="5%" right="5%">
+                        <Image
+                            src={jellyfish}
+                            alt="Jellyfish"
+                            width="314px"
+                            height="221px"
+                            zIndex="1"
+                            style={jellyfishStyle}
+                            onMouseEnter={() => {
+                                setJellyfishStyle(zoomOutOnHover);
+                            }}
+                            onMouseLeave={() => {
+                                setJellyfishStyle(zoomInOnHover);
+                            }}
+                        />
+                        <Text fontSize="22" fontWeight="400" fontFamily="Inknut Antiqua" wordBreak="break-word"> FLORENCE </Text>
+                    </Box>
+                </Flex>
+                <Flex direction="column">
+                    <Box position="absolute" top="2%" left="5%">
                 <Image
                     src={horsewheel}
                     alt="Horsewheel"
-                    position="absolute"
-                    top="2%"        
-                    left="5%"
-                    width="135.40px"     
+                    width="135.40px"
                     height="117.10px"
                     zIndex="1"
                 />
+                    <Text fontSize="22" fontWeight="400" fontFamily="Inknut Antiqua" wordBreak="break-word"> EXTRAS </Text>
+                    </Box>
+                </Flex>
+                <Flex> 
+                    <Box position="absolute" bottom="2%" right="5%">
                 <Image
-                    src={spartanhelmet}
+                    src={isHoveringHelmet? helmet_animated : spartanhelmet}
                     alt="Spartan"
-                    position="absolute"
-                    bottom="2%"     
-                    right="5%"
-                    width="251.582px"    
+                    width="251.582px"
                     height="280.858px"
                     zIndex="1"
+                    onMouseEnter={() => setIsHoveringHelmet(true)}
+                    onMouseLeave={() => setIsHoveringHelmet(false)}
+                    style={{
+                        transition: "0.5s", 
+                    }}
                 />
+                <Text fontSize="22" fontWeight="400" fontFamily="Inknut Antiqua" wordBreak="break-word"> FILM & PHOTOGRAPHY </Text>
+                </Box>
+                </Flex>
+                <Flex direction="column">
+                    <Box  position="absolute" bottom="23%" left="10%">
+                    <Image
+                            src={isHoveringSword ? sword_animated : swords}
+                            alt="Swords"
+                            width="141.403px"
+                            height="213.193px"
+                            zIndex="1"
+                            onMouseEnter={() => setIsHoveringSword(true)}
+                            onMouseLeave={() => setIsHoveringSword(false)}
+                            style={{
+                                transition: "0.5s", 
+                            }}
+                        />
+                  <Text fontSize="22" fontWeight="400" fontFamily="Inknut Antiqua" wordBreak="break-word"> THE QUIZ FEST </Text>
+                  </Box>
+                </Flex>
+                <Flex>
+                    <Box position="absolute" bottom="4%" left="27%">
                 <Image
-                    src={swords}
-                    alt="Swords"
-                    position="absolute"
-                    bottom="15%"                       
-                    left="15%"
-                    width="141.403px"      
-                    height="213.193px"
-                    zIndex="1"
-                />
-                <Image
-                    src={box}
+                    src={isHoveringBox ? box_animated : box}
                     alt="Box"
-                    position="absolute"
-                    bottom="3%"     
-                    left="30%"
-                    width="235px"     
+                    width="235px"
                     height="200px"
                     zIndex="1"
-                />
+                    onMouseEnter={() => setIsHoveringBox(true)}
+                            onMouseLeave={() => setIsHoveringBox(false)}
+                            style={{
+                                transition: "3s", 
+                            }}
+                />  
+                   <Text fontSize="22" fontWeight="400" fontFamily="Inknut Antiqua" wordBreak="break-word"> BEAU VISTA </Text>
+                </Box>
+                </Flex>
                 <Image
-                   src={seaweed}
-                   alt="Seaweed"
-                   position="absolute"
-                   bottom="3.5%"
-                   left="48.5%"
-                   width="300px"
-                   height="162px"
-                   zIndex="1" 
+                    src={seaweed}
+                    alt="Seaweed"
+                    position="absolute"
+                    bottom="3.5%"
+                    left="48.5%"
+                    width="300px"
+                    height="162px"
+                    zIndex="1"
                 />
-            </Box> 
+            </Box>
         </ChakraProvider>
     );
 }

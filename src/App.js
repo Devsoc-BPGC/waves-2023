fa56de71023ab72fcc7ca2aeabe3289dbb63c791
Aryan Bhosale <f20210274@goa.bits-pilani.ac.in>
2023-09-18 22:37:10 +0530

    overflowhide

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import Footer from './Components/Footer';
 import Events from './Components/Events/events';
+import './App.css';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Box className='app-container' textAlign='center' fontSize='xl'>
         <Events />
         <Footer />
       </Box>

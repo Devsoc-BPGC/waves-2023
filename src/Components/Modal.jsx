ccc3b3af0a79c0275ff120dbd75b3165fe1d86db
Abheshek Murugan <abheshek.murugan@gmail.com>
2023-09-26 01:08:27 +0530

    Added dotted outline and its animation

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,21 +3,21 @@ import { motion } from 'framer-motion';
 import { CloseButton, Image } from '@chakra-ui/react';
 import picture from '../assets/Waves_logo2.png';
 
-const style = bgColour => {
-  return {
-    backgroundColor: bgColour,
-    height: 0,
-    width: 0,
-    borderRadius: '50%',
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-};
-
 const Modal = props => {
+  const style = bgColour => {
+    return {
+      backgroundColor: bgColour,
+      height: 0,
+      width: 0,
+      borderRadius: '50%',
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
+  };
+
   const variants = {
     open: {
       height: '200vh',
@@ -57,7 +57,46 @@ const Modal = props => {
         width: `${r}vh`,
         transition: {
           duration: 0.5,
-          when: 'beforeChildren',
+          delayChildren: 0.2,
+        },
+      },
+      close: {
+        height: 0,
+        width: 0,
+        transition: { duration: 0.2, when: 'afterChildren' },
+      },
+    };
+  };
+
+  const circleVariant2 = r => {
+    return {
+      open: {
+        height: `${r}vh`,
+        width: `${r}vh`,
+        outline: '2px dotted #FFF',
+        transition: {
+          duration: 0.5,
+          delayChildren: 0.2,
+        },
+      },
+      close: {
+        height: 0,
+        width: 0,
+        transition: { duration: 0.2, when: 'afterChildren' },
+      },
+    };
+  };
+
+  const circleVariant3 = r => {
+    return {
+      open: {
+        height: `${r}vh`,
+        width: `${r}vh`,
+        outline: '2px dotted #FFF',
+        transition: {
+          delay: 1.3,
+          duration: 0.5,
+          delayChildren: 1.2,
         },
       },
       close: {
@@ -72,7 +111,7 @@ const Modal = props => {
     <motion.div
       animate='open'
       style={{
-        backgroundColor: '#48494a',
+        backgroundColor: 'rgba(137, 137, 137, 0.04)',
         position: 'absolute',
         height: 0,
         width: 0,
@@ -86,6 +125,59 @@ const Modal = props => {
       variants={variants}
       exit='close'
     >
+      <motion.div
+        style={{
+          zIndex: 3,
+          position: 'absolute',
+          top: '0px',
+          left: '3%',
+          height: '100%',
+          width: '94%',
+          backgroundColor: 'transparent',
+        }}
+      >
+        <motion.div
+          style={{
+            marginTop: '-50vh',
+            display: 'flex',
+            alignContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            height: '200vh',
+          }}
+        >
+          <motion.div
+            style={style('transparent')}
+            variants={circleVariant3(203)}
+          >
+            <motion.div
+              style={style('transparent')}
+              variants={circleVariant2(176)}
+            >
+              <motion.div
+                style={style('transparent')}
+                variants={circleVariant2(149)}
+              >
+                <motion.div
+                  style={style('transparent')}
+                  variants={circleVariant2(122)}
+                >
+                  <motion.div
+                    style={style('transparent')}
+                    variants={circleVariant2(95)}
+                  >
+                    <motion.div
+                      style={style('transparent')}
+                      variants={circleVariant2(68)}
+                    ></motion.div>
+                  </motion.div>
+                </motion.div>
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
       <CloseButton
         onClick={props.handleClick}
         style={{ position: 'absolute' }}
@@ -101,19 +193,28 @@ const Modal = props => {
           height: '200vh',
         }}
       >
-        <motion.div style={style('#5d5e5e')} variants={circleVariant(203)}>
-          <motion.div style={style('#797a7a')} variants={circleVariant(176)}>
-            <motion.div style={style('#9a9b9c')} variants={circleVariant(149)}>
+        <motion.div
+          style={style('rgba(217, 217, 217, 0.10)')}
+          variants={circleVariant(203)}
+        >
+          <motion.div
+            style={style('rgba(217, 217, 217, 0.15)')}
+            variants={circleVariant(176)}
+          >
+            <motion.div
+              style={style('rgba(217, 217, 217, 0.20)')}
+              variants={circleVariant(149)}
+            >
               <motion.div
-                style={style('#b6b6b8')}
+                style={style('rgba(217, 217, 217, 0.25)')}
                 variants={circleVariant(122)}
               >
                 <motion.div
-                  style={style('#cfd0d1')}
+                  style={style('rgba(217, 217, 217, 0.30)')}
                   variants={circleVariant(95)}
                 >
                   <motion.div
-                    style={style('#ededed')}
+                    style={style('rgba(217, 217, 217, 0.60)')}
                     variants={circleVariant(68)}
                   >
                     <Image src={picture} />

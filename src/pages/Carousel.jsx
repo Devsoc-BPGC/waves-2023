b8aea0741f69ae6b96b7c37dc89f673949fb9eca
arjit120604 <130384377+arjit120604@users.noreply.github.com>
2023-09-29 05:53:58 +0530

    added infinite scroll and horizontal scroll
    not final version, jitters after it loops

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -1,28 +1,63 @@
-import React from 'react';
-import { Center, Image } from '@chakra-ui/react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  Flex,
-} from '@chakra-ui/react';
+import React, { useEffect, useRef, useState } from 'react';
+import { ChakraProvider, Box, Image } from '@chakra-ui/react';
 import back_image from '../assets/back_image_page2.jpg';
-import ChakraCarousel from '../Components/ChakraCarousel';
-// import ImageSlider from '../Components/ImageSlider';
 import images from './images';
+import './Carousel.css';
 
 export default function Carousel() {
+  const containerRef = useRef(null);
+  const [isMouseOverCarousel, setIsMouseOverCarousel] = useState(false);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleScroll = event => {
+      if (isMouseOverCarousel && event.deltaMode === 0) {
+        const scrollAmount = event.deltaY;
+        const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+        if (container.scrollLeft + scrollAmount >= maxScrollLeft) {
+          requestAnimationFrame(() => {
+            container.scrollLeft = 0;
+          });
+          event.preventDefault();
+        } else if (container.scrollLeft + scrollAmount <= 0) {
+          requestAnimationFrame(() => {
+            container.scrollLeft = maxScrollLeft;
+          });
+          event.preventDefault();
+        } else {
+          container.scrollLeft += scrollAmount;
+          event.preventDefault();
+        }
+      }
+    };
+
+    const handleMouseEnter = () => {
+      setIsMouseOverCarousel(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsMouseOverCarousel(false);
+    };
+
+    container.addEventListener('wheel', handleScroll, { passive: false });
+    container.addEventListener('mouseenter', handleMouseEnter);
+    container.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      container.removeEventListener('wheel', handleScroll);
+      container.removeEventListener('mouseenter', handleMouseEnter);
+      container.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, [isMouseOverCarousel]);
+
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Box
         textAlign='center'
         fontSize='xl'
-        backgroundImage={back_image}
+        backgroundImage={`url(${back_image})`}
         backgroundSize='cover'
         h='100vh'
         display='flex'
@@ -30,14 +65,28 @@ export default function Carousel() {
         alignContent='center'
         justifyContent='center'
         alignItems='center'
+        overflow='hidden'
       >
-        {/* <Box marginTop='25%' margin='auto' h='20vh'> */}
-        <ChakraCarousel gap={32}>
-          {images.map(image => (
-            <Image src={image.src}></Image>
+        <div
+          ref={containerRef}
+          style={{
+            display: 'flex',
+            overflowX: 'auto',
+            width: '100%',
+            whiteSpace: 'nowrap',
+            marginTop: '10%',
+            gap: '3%',
+          }}
+        >
+          {images.map((image, index) => (
+            <Image
+              key={index}
+              src={image.src}
+              w={['100%', '100%', '35%']}
+              flex='1'
+            />
           ))}
-        </ChakraCarousel>
-        {/* </Box> */}
+        </div>
       </Box>
     </ChakraProvider>
   );

6a692f5d7d310fded83787519d903d6cbe57e806
tanish111 <tanishdesai37@gmail.com>
2023-10-06 14:15:19 +0530

    Merge All Pages + Routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { ChakraProvider, Box, theme, Show } from '@chakra-ui/react';
-import EventsMobile from './Components/Events/EventsMobile';
-import Events from './Components/Events/events';
-import './App.css';
-import HomePage from './Pages/HomePage';
-import './App.css';
-import Footer from './Components/Footer';
+import MainPage from './Pages/MainPage';
+import { Route, Routes } from 'react-router-dom';
+import TeamsPage from './Pages/TeamsPage';
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box className='app-container' textAlign='center' fontSize='xl'>
-        <div style={{ overflowX: 'hidden' }}>
-          <HomePage />
-        </div>
-        <Show above='750px'>
-          <Events />
-        </Show>
-        <Show below='749px'>
-          <EventsMobile />
-        </Show>
-        <Footer />
-      </Box>
-    </ChakraProvider>
+    <Routes>
+      <Route path='/' element={<MainPage />}></Route>
+      <Route path='/teams' element={<TeamsPage />}></Route>
+    </Routes>
   );
 }
 export default App;
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ChakraProvider, Box, theme, Show } from '@chakra-ui/react';
+import EventsMobile from '../Components/Events/EventsMobile';
+import Events from '../Components/Events/events';
+import HomePage from '../Pages/HomePage';
+import '../App.css';
+import Footer from '../Components/Footer';
+export default function MainPage() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box className='app-container' textAlign='center' fontSize='xl'>
+        <div style={{ overflowX: 'hidden' }}>
+          <HomePage />
+        </div>
+        <Show above='750px'>
+          <Events />
+        </Show>
+        <Show below='749px'>
+          <EventsMobile />
+        </Show>
+        <Footer />
+      </Box>
+    </ChakraProvider>
+  );
+}

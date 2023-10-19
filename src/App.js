cf683391435816cdfba8182a9fe0f4e3d265cfe4
tanish111 <tanishdesai37@gmail.com>
2023-10-20 01:21:13 +0530

    Sponsers

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 import TeamsPage from './Pages/TeamsPage';
 import MediaPage from './Pages/MediaPage';
 import Carousel from './Pages/Carousel';
-import Sponsors from './Pages/Sponsors';
+import SponsorsPage from './Pages/SponsorsPage';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />}></Route>
       <Route path='/teams' element={<TeamsPage />}></Route>
       <Route path='/media' element={<MediaPage />}></Route>
-      <Route path='/sponsors' element={<Sponsors />}></Route>
+      <Route path='/sponsors' element={<SponsorsPage />}></Route>
     </Routes>
   );
 }

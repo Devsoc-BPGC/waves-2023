e988713903886e071c44188a5245bc06118f9f6b
tanish111 <tanishdesai37@gmail.com>
2023-10-22 17:50:46 +0530

    Teams Responsive

--- src/Components/TeamMemberCard.jsx
+++ src/Components/TeamMemberCard.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Flex, Image, Text } from '@chakra-ui/react';
+import { Flex, Image, Text, useMediaQuery } from '@chakra-ui/react';
 
 export default function TeamMemberCard(props) {
+  const [isSmallerThan600] = useMediaQuery('(max-width: 600px)');
   return (
     <Flex
       flexDir='column'
-      style={{ height: '30rem', width: '25rem' }}
+      style={{
+        height: isSmallerThan600 ? '23rem' : '30rem',
+        width: isSmallerThan600 ? '17rem' : '25rem',
+      }}
       bgColor='rgba(0, 0, 0, 0.22)'
       alignItems='center'
       borderRadius='20px'
       backdropFilter='auto'
       backdropBlur='6px'
+      margin='5px'
     >
       <Image
         src={props.img}
@@ -19,10 +24,20 @@ export default function TeamMemberCard(props) {
         objectFit='fill'
         pt='20px'
       ></Image>
-      <Text fontFamily='Junge' fontSize='40' p='13px' color='white'>
+      <Text
+        fontFamily='Junge'
+        fontSize={isSmallerThan600 ? '33' : '40'}
+        p='13px'
+        color='white'
+      >
         {props.name}
       </Text>
-      <Text fontFamily='Junge' fontSize='32' p='13px' color='white'>
+      <Text
+        fontFamily='Junge'
+        fontSize={isSmallerThan600 ? '27' : '32'}
+        p='13px'
+        color='white'
+      >
         {props.position}
       </Text>
     </Flex>
